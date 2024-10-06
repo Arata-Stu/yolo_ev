e418f7657f66f91f65f505828b6f9dd5f9205c65
Arata Tanaka <ky.37f.9850@s.thers.ac.jp>
2024-10-07 01:58:06 +0900

    add images

--- yolo_ev/module/data/dataset/dsec_det/dsec_det_dataset.py
+++ yolo_ev/module/data/dataset/dsec_det/dsec_det_dataset.py
@@ -1,4 +1,5 @@
 import numpy as np
+import cv2
 import torch
 from torch.utils.data import Dataset
 from pathlib import Path
@@ -8,16 +9,18 @@ from .io import extract_from_h5_by_timewindow
 
 class DsecDetDataset(Dataset):
 
-    def __init__(self, data_dir, split_config, img_size=(256, 256), transform=None, sync="back"):
+    def __init__(self, data_dir, split_config, img_size=(256, 256), transform=None, sync="back", use_events=True, use_imgs=True):
         self.data_dir = Path(data_dir)
         self.split_config = split_config
-        self.img_size=img_size
+        self.img_size = img_size
         self.transform = transform
         self.sync = sync
+        self.use_events = use_events
+        self.use_imgs = use_imgs
 
         available_dirs = list(self.data_dir.glob("*/"))
         self.subsequence_directories = [self.data_dir / s for s in split_config if self.data_dir / s in available_dirs]
-        
+
         self.subsequence_directories = sorted(self.subsequence_directories, key=self.first_time_from_subsequence)
 
         self.directories = dict()
@@ -30,25 +33,37 @@ class DsecDetDataset(Dataset):
                 directory.images.timestamps
             )
 
-
     def __len__(self):
         return sum(len(v) - 1 for v in self.img_idx_track_idxs.values())
 
-
     def __getitem__(self, item):
         output = {}
-        output['events'] = self.get_events(item)
+        
+        if self.use_imgs:
+            output['image'] = self.get_image(item)
+        
+        if self.use_events:
+            output['events'] = self.get_events(item)
+        
         output['tracks'] = self.get_tracks(item)
         output['img_info'] = (640, 640)
         output['img_id'] = 0
 
         if self.transform is not None:
             img, target, info, id = self.transform(output) 
+        else:
+            img, target, info, id = output['image'], output['tracks'], output['img_info'], output['img_id']
 
         return img, target, info, id
     
     def first_time_from_subsequence(self, subsequence):
         return np.genfromtxt(subsequence / "images/timestamps.txt", dtype="int64")[0]
+    
+    def get_image(self, index, directory_name=None):
+        index, img_idx_to_track_idx, directory = self.rel_index(index, directory_name)
+        image_files = directory.images.image_files_distorted
+        image = cv2.imread(str(image_files[index]))
+        return image
     
     def get_tracks(self, index, mask=None, directory_name=None):
         index, img_idx_to_track_idx, directory = self.rel_index(index, directory_name)
@@ -80,7 +95,6 @@ class DsecDetDataset(Dataset):
 
         return i_0, i_1
 
-    
     def rel_index(self, index, directory_name=None):
         if directory_name is not None:
             img_idx_to_track_idx = self.img_idx_track_idxs[directory_name]
--- yolo_ev/module/data/dataset/dsec_det/transform.py
+++ yolo_ev/module/data/dataset/dsec_det/transform.py
@@ -15,23 +15,38 @@ def render_events_on_empty_image(height, width, x, y, p):
             image[y_, x_] = np.array([255, 0, 0])  # ONイベントは赤
     return image
 
+@numba.jit(nopython=True)
+def render_events_on_image(image, x, y, p):
+    # 画像にイベントを描画
+    for x_, y_, p_ in zip(x, y, p):
+        if p_ == 0:
+            image[y_, x_] = np.array([0, 0, 255])  # OFFイベントは青
+        else:
+            image[y_, x_] = np.array([255, 0, 0])  # ONイベントは赤
+    return image
+
+
 class TrainTransform:
     def __init__(self, max_labels=50, flip_prob=0.5, hsv_prob=1.0, img_size=(640,640)):
         self.max_labels = max_labels
         self.flip_prob = flip_prob
         self.hsv_prob = hsv_prob
-
         self.img_size = img_size
 
     def __call__(self, inputs):
         events = inputs['events']
         tracks = inputs['tracks']
-
         height, width = self.img_size
-        # イベントフレームの作成 (例)
-        event_frame = render_events_on_empty_image(height, width, events['x'], events['y'], events['p'])
-        event_frame = np.transpose(event_frame, (2, 0, 1))  # (channels, height, width)
 
+        # imagesがある場合は、その上にイベントを描画
+        if 'image' in inputs and inputs['image'] is not None:
+            image = inputs['image']
+            event_frame = render_events_on_image(image, events['x'], events['y'], events['p'])
+        else:
+            # イベントフレームの作成 (例)
+            event_frame = render_events_on_empty_image(height, width, events['x'], events['y'], events['p'])
+
+        event_frame = np.transpose(event_frame, (2, 0, 1))  # (channels, height, width)
 
         # 複数のボックスに対応するために、各トラックデータから x, y, w, h を抽出
         bboxes = np.stack((tracks['x'], tracks['y'], tracks['w'], tracks['h']), axis=-1)
@@ -50,24 +65,29 @@ class TrainTransform:
         padded_labels = np.ascontiguousarray(padded_labels, dtype=np.float32)
 
         return event_frame, padded_labels, inputs['img_info'], inputs['img_id']
+
     
 class ValTransform:
     def __init__(self, max_labels=50, flip_prob=0.5, hsv_prob=1.0, img_size=(640,640)):
         self.max_labels = max_labels
         self.flip_prob = flip_prob
         self.hsv_prob = hsv_prob
-
         self.img_size = img_size
 
     def __call__(self, inputs):
         events = inputs['events']
         tracks = inputs['tracks']
-
         height, width = self.img_size
-        # イベントフレームの作成 (例)
-        event_frame = render_events_on_empty_image(height, width, events['x'], events['y'], events['p'])
-        event_frame = np.transpose(event_frame, (2, 0, 1))  # (channels, height, width)
 
+        # imagesがある場合は、その上にイベントを描画
+        if 'image' in inputs and inputs['image'] is not None:
+            image = inputs['image']
+            event_frame = render_events_on_image(image, events['x'], events['y'], events['p'])
+        else:
+            # イベントフレームの作成 (例)
+            event_frame = render_events_on_empty_image(height, width, events['x'], events['y'], events['p'])
+
+        event_frame = np.transpose(event_frame, (2, 0, 1))  # (channels, height, width)
 
         # 複数のボックスに対応するために、各トラックデータから x, y, w, h を抽出
         bboxes = np.stack((tracks['x'], tracks['y'], tracks['w'], tracks['h']), axis=-1)
@@ -79,7 +99,7 @@ class ValTransform:
         targets_t = np.hstack((bboxes_xyxy, tracks['class_id'].reshape(-1, 1)))
 
         # padded_labels の作成
-        padded_labels = np.zeros((self.max_labels, 5))  # 
+        padded_labels = np.zeros((self.max_labels, 5))
         padded_labels[:min(len(targets_t), self.max_labels)] = targets_t[:self.max_labels]
 
         # 必要に応じてデータ型を調整
